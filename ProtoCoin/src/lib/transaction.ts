53701b74ce0dbf1d3603b431909aaa5a04ccd545
rramires <ricardo@flexbr.com>
2023-07-11 20:38:50 -0300

    Add transactionOutput class and initial fixes

--- ProtoCoin/src/lib/transaction.ts
+++ ProtoCoin/src/lib/transaction.ts
@@ -2,6 +2,8 @@ import { SHA256 } from "crypto-js";
 import Validation from "./validation";
 import TransactionType from "./transactionType";
 import TransactionInput from "./transactionInput";
+import TransactionOutput from "./transactionOutput";
+import Blockchain from "./blockchain";
 
 /**
  * Transaction class
@@ -14,21 +16,28 @@ export default class Transaction{
     /** Invalid timestamp */
     static INVALID_TIMESTAMP: Validation = new Validation(false, "Invalid timestamp."); 
 
-    /** Invalid data */
-    //static INVALID_DATA: Validation = new Validation(false, "Invalid data."); 
-
     /** Invalid hash */
     static INVALID_HASH: Validation = new Validation(false, "Invalid hash."); 
 
+    /** Invalid TXO. */
+    static INVALID_TXO: Validation = new Validation(false, "Invalid TXO."); 
+
+    /** Invalid TXO reference hash. */
+    static INVALID_TXO_REF_HASH: Validation = new Validation(false, "Invalid TXO reference hash."); 
+   
     /** `Invalid input tx: {msg} */
     static INVALID_INPUT_TX(msg: string): Validation{
         return new Validation(false, `Invalid input tx: ${msg}`);
     }
 
+    /** "Invalid tx: output greater than input." */
+    static INVALID_TXO_GREATER: Validation = new Validation(false, "Invalid tx: output greater than input."); 
+
+
     type: TransactionType;
     timestamp: number;
-    txInputs: TransactionInput | undefined;
-    txOutputs: string;
+    txInputs: TransactionInput[] | undefined;
+    txOutputs: TransactionOutput[];
     hash: string;
 
     /**
@@ -38,11 +47,16 @@ export default class Transaction{
     constructor(tx?: Transaction){
         this.type = tx?.type || TransactionType.REGULAR;
         this.timestamp = tx?.timestamp || Date.now();
-        /* c8 ignore next */
-        this.txInputs = tx && tx.txInputs ? new TransactionInput(tx?.txInputs) : undefined;
-        //
-        this.txOutputs = tx?.txOutputs || "";
+        
+        // map sub objects
+        this.txInputs = tx?.txInputs ? tx.txInputs.map(txi => new TransactionInput(txi)) : undefined;
+        this.txOutputs = tx?.txOutputs ? tx.txOutputs.map(txo => new TransactionOutput(txo)) : [];
+
+        // hash this
         this.hash = tx?.hash || this.getHash();
+
+        // apply tx hash in all outputs
+        this.txOutputs.forEach((txo, index, arr) => arr[index].tx = this.hash);
     }
 
     /**
@@ -50,10 +64,16 @@ export default class Transaction{
      * @returns string - hash
      */
     getHash(): string{
-        const from = this.txInputs ? this.txInputs.getHash() : "";
+        // concat hashes
+        const from = this.txInputs && this.txInputs.length ? 
+                        this.txInputs.map(txi => txi.signature).join(",") : "";
+        const to = this.txOutputs && this.txOutputs.length ? 
+                        this.txOutputs.map(txi => txi.getHash()).join(",") : "";
+        // hash all
         return SHA256(this.type +
                       this.timestamp +
-                      from +
+                      from + 
+                      to +
                       this.txOutputs).toString();
     }
 
@@ -65,11 +85,31 @@ export default class Transaction{
         if(this.timestamp < 0) return Transaction.INVALID_TIMESTAMP;
         //if(!this.txOutputs) return Transaction.INVALID_DATA;
         if(this.hash !== this.getHash()) return Transaction.INVALID_HASH;
-        // optional
-        if(this.txInputs){
-            const validation = this.txInputs.isValid();
-            if(!validation.success) return Transaction.INVALID_INPUT_TX(validation.message);
+
+        // Check TXO's
+        // check reference hash
+        if (this.txOutputs.some(txo => txo.tx !== this.hash)) return Transaction.INVALID_TXO_REF_HASH;
+        // check if is valid
+        if (!this.txOutputs || !this.txOutputs.length || this.txOutputs.map(txo => txo.isValid()).some(v => !v.success)){
+            return Transaction.INVALID_TXO;
         }
+            
+        // Check TXI's
+        if (this.txInputs && this.txInputs.length) {
+            // validate all inputs
+            const validations = this.txInputs.map(txi => txi.isValid()).filter(v => !v.success);
+            if (validations && validations.length) {
+                const message = validations.map(v => v.message).join(" ");
+                return Transaction.INVALID_INPUT_TX(message);
+            }
+            // check if output greater than input
+            const inputSum = this.txInputs.map(txi => txi.amount).reduce((a, b) => a + b, 0);
+            const inputOutput = this.txOutputs.map(txo => txo.amount).reduce((a, b) => a + b, 0);
+            if (inputSum < inputOutput) return Transaction.INVALID_TXO_GREATER;
+        }
+
+        // TODO: Validate TX FEE
+        
         //
         // else - success
         return Transaction.VALID_TRANSACTION;
---
+++ ProtoCoin/src/lib/transactionOutput.ts
@@ -0,0 +1,39 @@
+import Validation from "./validation";
+import sha256 from 'crypto-js/sha256';
+
+/**
+ * Transaction Output class
+ */
+export default class TransactionOutput {
+
+
+    /** Invalid negative amount. */
+    static INVALID_NEGATIVE_AMOUNT: Validation = new Validation(true, "Invalid, negative amount.");
+
+    /** Valid transaction output. */
+    static VALID_TX_OUTPUT: Validation = new Validation(true, "Valid transaction output.");
+
+    toAddress: string;
+    amount: number;
+    tx?: string;
+
+    /**
+     * Creates a new TransactionOutput
+     * @param txOutput - TransactionOutput optional
+     */
+    constructor(txOutput?: TransactionOutput) {
+        this.toAddress = txOutput?.toAddress || "";
+        this.amount = txOutput?.amount || 0;
+        this.tx = txOutput?.tx || "";
+    }
+
+    isValid(): Validation{
+        if (this.amount < 1) return TransactionOutput.INVALID_NEGATIVE_AMOUNT;
+
+        return TransactionOutput.VALID_TX_OUTPUT;
+    }
+
+    getHash(): string{
+        return sha256(this.toAddress + this.amount).toString();
+    }
+}
