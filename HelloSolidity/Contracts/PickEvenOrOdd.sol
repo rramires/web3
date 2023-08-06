c18bbba270bcdbda9229856c3969525226dd32fe
rramires <ricardo@flexbr.com>
2023-08-06 17:41:53 -0300

    Modify to double player - EvenOrOdd v3

--- HelloSolidity/Contracts/PickEvenOrOdd.sol
+++ HelloSolidity/Contracts/PickEvenOrOdd.sol
@@ -7,13 +7,9 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PickEvenOrOdd{
 
-    string public  choice = ""; // EVEN or ODD
-
-    /*
-    compare two strings
-    *** In solidity string is an array of bytes, similar to C.
-        There is no way to compare using "a" == "b"
-    */
+    /**
+     * Compare two strings
+     */
     function compareString(string memory str1, string memory str2) private pure returns(bool) {
         bytes memory arrA = bytes(str1);
         bytes memory arrB = bytes(str2);
@@ -21,61 +17,86 @@ contract PickEvenOrOdd{
     }
 
 
+    // Player 1
+    // address
+    address public player1;
+    // EVEN or ODD choice
+    string public  choicePlayer1 = ""; 
+    // Player 1 move
+    uint private movePlayer1;
+
+    // status of game
+    string public gameStatus;
+
+    
+
     /**
-     * newChoice EVEN or ODD
+     * Player 1 - choice EVEN or ODD 
      */
     function choose(string memory newChoice) public{
 
-        /* 
-         Use require for validation
-         require reverts the transaction
-        */
+        // Checks if player 1 is choosing again
+        string memory message = string.concat("Player 1 already choose: ", choicePlayer1);
+        require(compareString(choicePlayer1, ""), message);
+
+        // Checks whether EVEN or ODD was chosen
         require(compareString(newChoice, "EVEN") || compareString(newChoice, "ODD"), "Choose: EVEN or ODD");
+    
+        // Set choice
+        choicePlayer1 = newChoice;
 
-        // set
-        choice = newChoice;
+        // Set caller address
+        player1 = msg.sender;
+
+        // Change status
+        gameStatus = string.concat("Player 1 is ", Strings.toHexString(player1), " and choose ", choicePlayer1, ".");
     }
 
 
-    /*
-    *** IMPORTANT: The purpose is didactic! This could be a security hole.
-        It is possible to "guess" a random number in an EVM due to slowness.
-        Returns 0 or 1 by % 2
-    */
-    function random() private view returns(uint) {
-        return uint( keccak256( abi.encodePacked(block.timestamp, choice) ) ) % 2;
-    }
+    /**
+     * Player 1 or 2 - move 1 or 2 values
+     */
+    function play(uint8 move) public {
 
+        // Choose validation
+        require(!compareString(choicePlayer1, ""), "First, Player 1 must choose: EVEN or ODD");
 
-    function play(uint8 number) public view returns(string memory) {
+        
+        // Checks if player 1 or 2 is playing
+        if(msg.sender == player1){ 
+            // If player 1
+            movePlayer1 = move;
 
-        // param validation
-        require(number >= 0 && number <= 2, "Play with: 0, 1 or 2");
-
-        // choose validation
-        require(!compareString(choice, ""), "First, choose: EVEN or ODD");
-
-        // get CPU ramdom option
-        uint cpuNumber = random();
-
-        // validation 
-        bool isEven = (cpuNumber + number) % 2 == 0;
-
-        // return message
-        string memory message = string.concat("Player choose: ", choice, 
-                                              ", and plays: ", Strings.toString(number), 
-                                              ". CPU plays: ", Strings.toString(cpuNumber));
-
-        // returns
-        if(isEven && compareString(choice, "EVEN")){
-            return string.concat(message, " Player WON!");
-        }
-        else if(!isEven && compareString(choice, "ODD")) {
-            return string.concat(message, " Player WON!");
+            // Change status
+            gameStatus = "Player 1 already played. Waiting for player 2's move.";
         }
         else{
-            return string.concat(message, " CPU WON!");
-        }        
+            // If player 2
+
+            // Decides whether it's even or odd
+            bool isEven = (movePlayer1 + move) % 2 == 0;
+
+            // Return message
+            string memory message = string.concat("Player 1 choose: ", choicePlayer1, 
+                                                ", and plays: ", Strings.toString(movePlayer1), 
+                                                ". Player 2, plays: ", Strings.toString(move));
+
+            // returns
+            if(isEven && compareString(choicePlayer1, "EVEN")) {
+                gameStatus = string.concat(message, " - Player 1 WON!");
+            }
+            else if(!isEven && compareString(choicePlayer1, "ODD")) {
+                gameStatus = string.concat(message, " - Player 1 WON!");
+            }
+            else {
+                gameStatus = string.concat(message, " - Player 2 WON!");
+            }  
+
+            // clear for next game
+            player1 = address(0);
+            choicePlayer1 = ""; 
+            movePlayer1 = 0;
+        }   
     }
 }
 
