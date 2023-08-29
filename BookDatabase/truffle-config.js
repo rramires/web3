412f0a36041b38b7e4d3c75c64bf89c886df5811
rramires <ricardo@flexbr.com>
2023-08-29 19:07:57 -0300

    Add Deploy via Infura

--- BookDatabase/truffle-config.js
+++ BookDatabase/truffle-config.js
@@ -1,9 +1,22 @@
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   networks: {
     wsl: {
       host: "172.17.192.1", // WSL IP (Ganache hostname changed to vEthernet(WSL))
       port: 7545,
       network_id: "5777"
+    },
+    goerli: {
+      provider: new HDWalletProvider({
+        mnemonic: {
+          phrase: process.env.SECRET
+        },
+        providerOrUrl: process.env.INFURA_URL
+      }),
+      network_id: "5"
     } 
   },
   compilers: {
