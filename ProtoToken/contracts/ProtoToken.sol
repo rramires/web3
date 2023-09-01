77a381d5d2e00205dfde18bf009850fb0f715f60
rramires <ricardo@flexbr.com>
2023-08-31 21:00:09 -0300

    Added first ERC-20 properties and tests

--- ProtoToken/contracts/ProtoToken.sol
+++ ProtoToken/contracts/ProtoToken.sol
@@ -4,4 +4,27 @@ pragma solidity ^0.8.17;
 
 contract ProtoToken{
 
+    /**
+     * Token Name
+     */
+    string public name = "ProtoToken";
+
+    /**
+     * Token Symbol
+     */
+    string public symbol = "PROTK";
+
+    /**
+     * Number of decimals
+     */
+    uint8 public decimals = 18;
+
+    /**
+     * Total supply
+     * Number of coinn * decimal places ex: 21 million 
+     */
+    uint256 public totalSupply = 21000000 * 10 ** decimals;
+
+
+
 }
--- ProtoToken/test/ProtoToken.test.js
+++ ProtoToken/test/ProtoToken.test.js
@@ -1,14 +1,46 @@
+const BN = require("bn.js");
+
 const ProtoToken = artifacts.require("ProtoToken");
 
 contract('ProtoToken', function(accounts) {
+
+  // aux
+  const DECIMALS = new BN(18);
+
 
   // create contract before each test
   beforeEach(async() => {
     contract = await ProtoToken.new();
   })
 
-  it("Should ... ", async () => {
+
+  it("Should has correct name", async () => {
+    // get 
+    const name = await contract.name();
     // validate 
-    assert(true, "Error Message");
+    assert(name == "ProtoToken", "Incorrect name.");
+  })
+
+  it("Should has correct symbol", async () => {
+    // get 
+    const symbol = await contract.symbol();
+    // validate 
+    assert(symbol == "PROTK", "Incorrect symbol.");
+  })
+
+  it("Should has correct decimals", async () => {
+    // get 
+    const decimals = await contract.decimals(); // returns big number
+    // validate 
+    assert(decimals.eq(DECIMALS), "Incorrect decimals."); // uses big number eq to compare
+  })
+
+  it("Should has correct total supply", async () => {
+    // set 21 millions of coins
+    const TOTAL_SUPPLY = new BN(21000000).mul(new BN(10).pow(DECIMALS));
+    // get 
+    const totalSupply = await contract.totalSupply();
+    // validate 
+    assert(totalSupply.eq(TOTAL_SUPPLY), "Incorrect totalSupply.");
   })
 })
