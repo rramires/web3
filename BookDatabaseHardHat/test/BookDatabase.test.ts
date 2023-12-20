c9925b0779b9e8f7ca7385bc7ee5c4db265dab19
rramires <ricardo@flexbr.com>
2023-12-20 15:15:47 -0300

    Add BookDatabase tests

--- BookDatabaseHardHat/test/BookDatabase.test.ts
+++ BookDatabaseHardHat/test/BookDatabase.test.ts
@@ -7,6 +7,13 @@ describe("BookDatabase", function () {
   // Deploy template
   async function deployFixture() {
 
+
+    // Aux
+    const bookSample = {
+        title: "Learning Solidity",
+        year: 2022
+    }
+
     // Get owner and other accounts
     const [owner, otherAccount] = await ethers.getSigners();
 
@@ -17,7 +24,7 @@ describe("BookDatabase", function () {
     const bookDatabase = await BookDatabase.deploy();
 
     // Return the contract, owner and other accounts
-    return { bookDatabase, owner, otherAccount };
+    return { bookDatabase, owner, otherAccount, bookSample };
   }
 
   it("Should TOTAL = 0", async function () {
@@ -31,6 +38,68 @@ describe("BookDatabase", function () {
     expect(total).to.equal(0);
   });
 
-  
+  it("Should ADD book", async function () {
+    // Get contract
+    const { bookDatabase, owner, otherAccount, bookSample } = await loadFixture(deployFixture);
+
+    // Add book
+    await bookDatabase.addBook( bookSample );
+
+    // Get total
+    const total = await bookDatabase.total();
+
+    // Test
+    expect(total).to.equal(1);
+  });
+
+  it("Should EDIT book", async function () {
+    // Get contract
+    const { bookDatabase, owner, otherAccount, bookSample } = await loadFixture(deployFixture);
+
+    // Add book
+    await bookDatabase.addBook( bookSample );
+
+    // clone bookSample and change title
+    const editedBook = { ...bookSample };
+    editedBook.title = "Learning Solidity Edited";
+
+    // Edit book
+    await bookDatabase.editBook(1, editedBook);
+
+    // Get book
+    const book = await bookDatabase.books(1)
+
+    // Test
+    expect(book.title).to.equal(editedBook.title);
+  });
+
+  it("Should REMOVE book", async function () {
+    // Get contract
+    const { bookDatabase, owner, otherAccount, bookSample } = await loadFixture(deployFixture);
+
+    // Add book
+    await bookDatabase.addBook( bookSample );
+
+    // Edit book
+    await bookDatabase.removeBook(1);
+
+    // Get total
+    const total = await bookDatabase.total();
+
+    // Test
+    expect(total).to.equal(0);
+  });
+
+  it("Should NOT REMOVE book(restrict)", async function () {
+    // Get contract
+    const { bookDatabase, owner, otherAccount, bookSample } = await loadFixture(deployFixture);
+
+    // Connect to other account (default is owner)
+    const instance = bookDatabase.connect(otherAccount);
+
+    // Test with revert transaction
+    // Edit book (only the owner accesses this method because the "restricted" modifier has been applied)
+    await expect(instance.removeBook(1)).to.be.revertedWith("You don't have permission.");
+  });
 
 });
