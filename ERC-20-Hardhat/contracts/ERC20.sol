843c6a3e75916a81d381bcb963a506b6e432e090
rramires <ricardo@flexbr.com>
2024-01-06 11:38:46 -0300

    ERC-20 add approve function

--- ERC-20-Hardhat/contracts/ERC20.sol
+++ ERC-20-Hardhat/contracts/ERC20.sol
@@ -34,9 +34,24 @@ contract ERC20 {
     mapping(address => uint256) private _balances;
 
     /**
+     * Allowances structure
+     * 
+     * Wallet addr1 allows addr2 to transfer until 10 tokens
+     * 1:N eg:
+     * [addr1][addr2] = 10
+     * [addr1][addr3] = 20
+     */
+    mapping( address => mapping(address => uint256) ) private _allowances;
+
+    /**
      * Transfer event
      */
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    /**
+     * Approval event
+     */
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 
     /**
@@ -68,6 +83,19 @@ contract ERC20 {
         
         // Emits the transfer event
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    /**
+     * Approves delegated balance transfer
+     */
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        // add 1:N = [addr1][addr2] = 10
+        _allowances[msg.sender][_spender] = _value;
+
+        // Emits the approves event
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
