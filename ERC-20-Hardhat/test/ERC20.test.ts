56da032c9717d63c4c97d06727e0b7cab8d17769
rramires <ricardo@flexbr.com>
2024-01-05 21:19:14 -0300

    ERC-20 add transfer test

--- ERC-20-Hardhat/test/ERC20.test.ts
+++ ERC-20-Hardhat/test/ERC20.test.ts
@@ -13,62 +13,87 @@ describe("ERC20 Tests", function () {
     const ERC20 = await ethers.getContractFactory("ERC20");
 
     // Deploy
-    const erc20 = await ERC20.deploy();
+    const contract = await ERC20.deploy();
+
+    // Total supply
+    const total = 21000000n * 10n ** 18n
 
     // Return the contract, owner and other accounts
-    return { erc20, owner, otherAccount };
+    return { contract, total, owner, otherAccount };
   }
 
   // Tests
 
   it("Should have correct name", async function () {
-    const { erc20, owner, otherAccount } = await loadFixture(deployFixture);
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
 
     // get 
-    const name = await erc20.name();
+    const name = await contract.name();
 
     // Test
     expect(name).to.equal("ERCSample");
   });
   
   it("Should have correct symbol", async function () {
-    const { erc20, owner, otherAccount } = await loadFixture(deployFixture);
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
 
     // get 
-    const symbol = await erc20.symbol();
+    const symbol = await contract.symbol();
 
     // Test
     expect(symbol).to.equal("ERCS");
   });
 
   it("Should have correct decimals", async function () {
-    const { erc20, owner, otherAccount } = await loadFixture(deployFixture);
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
 
     // get 
-    const decimals = await erc20.decimals();
+    const decimals = await contract.decimals();
 
     // Test
     expect(decimals).to.equal(18);
   });
 
   it("Should have correct total supply", async function () {
-    const { erc20, owner, otherAccount } = await loadFixture(deployFixture);
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
 
     // get 
-    const totalSupply = await erc20.totalSupply();
+    const totalSupply = await contract.totalSupply();
 
     // Test
-    expect(totalSupply).to.equal(21000000n * 10n ** 18n);
+    expect(totalSupply).to.equal(total);
   });
 
   it("Should get balance", async function () {
-    const { erc20, owner, otherAccount } = await loadFixture(deployFixture);
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
 
     // get 
-    const balance = await erc20.balanceOf(owner);
+    const balance = await contract.balanceOf(owner.address);
 
     // Test
-    expect(balance).to.equal(21000000n * 10n ** 18n);
+    expect(balance).to.equal(total);
   });
 
+  it("Should transfer", async function () {
+    const { contract, total, owner, otherAccount } = await loadFixture(deployFixture);
+
+    // set 
+    const qty = 1n;
+
+    // get before
+    const fromBalanceBefore = await contract.balanceOf(owner.address);
+    const toBalanceBefore = await contract.balanceOf(otherAccount.address);
+
+    // transfer
+    await contract.transfer(otherAccount.address, qty);
+
+    const fromBalanceAfter = await contract.balanceOf(owner.address);
+    const toBalanceAfter = await contract.balanceOf(otherAccount.address);
+
+    // Tests
+    expect(fromBalanceBefore).to.equal(total);
+    expect(toBalanceBefore).to.equal(0n);
+    expect(fromBalanceAfter).to.equal(total -1n);
+    expect(toBalanceAfter).to.equal(1n);
+  });
 });
