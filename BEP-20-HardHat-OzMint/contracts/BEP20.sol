d32821cd785205e499675c862d3cf646f9272c71
rramires <ricardo@flexbr.com>
2024-01-08 16:39:50 -0300

    BEP-20 add mint functions

--- BEP-20-HardHat-OzMint/contracts/BEP20.sol
+++ BEP-20-HardHat-OzMint/contracts/BEP20.sol
@@ -9,13 +9,59 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // Inheritance in Solidity is done with: "is"
 contract BEP20 is ERC20{
 
+    // Contract owner
+    address private _owner;
+
+    // Amount of mint 
+    uint private _mintAmount = 0;
+
+    // 1 day delay 
+    uint64 private _mintDelay = 60 * 60 * 24; 
+
+    // Stores address + delay
+    mapping(address => uint256) private nextMint;
+
+
     // To call the parent's constructor, simply call 
     // the function with the same name: ERC20(args)
     constructor() ERC20("BEPSample", "BEPS"){
 
+        // Stores the contract owner
+        _owner = msg.sender;
+
         // Mint is the process of generating coins
         // Msg.sender is contract creator
-        _mint(msg.sender, 21000000 * 10 ** decimals());
+        _mint(_owner, 21000000 * 10 ** decimals());
+    }
+
+
+    // Modifier to restrict access to contract owner only
+    modifier restricted(){
+        // Validate
+        require(_owner == msg.sender, "You do not have permission.");
+        _;
+    }
+
+    // Change mint amount
+    function setMintAmount(uint newAmount) public restricted{
+        _mintAmount = newAmount;
+    }
+
+    // Change mint delay
+    function setMintDelay(uint64 newDelayInSeconds) public restricted{
+        _mintDelay = newDelayInSeconds;
     }
     
+    // Mint new tokens
+    function mint() public {
+        // Validate
+        require(_mintAmount > 0, "Minting is not enabled.");
+        require(block.timestamp > nextMint[msg.sender], "You cannot mint twice in a row.");
+
+        // Generate and send tokens
+        _mint(msg.sender, _mintAmount);
+
+        // Add address to delay mapping
+        nextMint[msg.sender] = block.timestamp + _mintDelay;
+    }
 }
